9afedb9862a38841fe976c24bba2dfbfe068cbbf
psrb191921 <me@mz.ax>
2021-10-10 15:05:39 +0300

    RPCServer

--- masternode/masternode.js
+++ masternode/masternode.js
@@ -1,71 +1,30 @@
-const http = require("http");
+const RPCServer = require("./rpcserver.js").RPCServer;
 
 class MasterNode {
-	constructor(ip = "0.0.0.0", port = 5555) {
+	constructor(ip = "0.0.0.0", rpcPort = 5555) {
 		this.ip = ip;
-		this.port = port;
-		this.rpcHandlers = {};
+		this.rpcPort = rpcPort;
+		this.rpcServer = new RPCServer(ip, rpcPort, this);
 		this.masterNodes = {};
 		this.nodes = {};
 		return;
 	}
 
-	addRpcHandler(path, handler) {
-		this.rpcHandlers[path] = handler;
-		return;
-	}
-
-	getRpcHandler(path) {
-		return this.rpcHandlers[path];
-	}
-
-	setupRpcHandlers() {
-		this.addRpcHandler("/info", this.onRpcInfo);
-		this.addRpcHandler("/register", this.onRpcRegister);
-		return;
-	}
-
-	createRpcServer() {
-		let server = http.createServer(
-			this.onRpcRequest.bind(this)
-		);
-		server.listen(
-			this.port
-		);
-		return server;
-	}
-
 	start() {
-		this.setupRpcHandlers();
-		this.rpcServer = this.createRpcServer();
+		this.rpcServer.start();
 		return;
 	}
 
-	onRpcRequest(request, response) {
-		try {
-			let handler = this.getRpcHandler(request.url);
-			if (!handler) {
-				throw { "error": "nosuchhandler" };
-			}
-			return handler(request, response);
-		}
-		catch (error) {
-			response.end(
-				JSON.stringify(error)
-			);
-		}
-		return;
+	getInfo(query) {
+		return {
+			"version": 0.1
+		};
 	}
 
-	onRpcInfo(request, response) {
-		response.end(
-			JSON.stringify({ "version": 0.1 })
-		);
-		return;
-	}
-
-	onRpcRegister(request, response) {
-		return;
+	addNode(query) {
+		return {
+			"result": "ok"
+		};
 	}
 }
 
